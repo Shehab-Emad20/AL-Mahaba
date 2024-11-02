2cb2426a0853b31097fc6de94278da9bd5e14521
Shehab-Emad20 <shehabz2014@gmail.com>
2024-11-02 07:57:33 +0200

    create folder utlils and add images

---
+++ lib/utils/app_images.dart
@@ -0,0 +1,11 @@
+class Assets {
+  Assets._();
+
+  static const String imagesfeed = "assets/images/feed.png";
+
+  static const String imagesinfo = "assets/images/info.png";
+
+  static const String imageslogo = "assets/images/logo.png";
+
+  static const String imagesmahaba = "assets/images/mahaba.png";
+}
